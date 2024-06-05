430c897147f3e33868ec9ad9f6df4f72efcea2f3
Capiru <gabriel.silva.2@deliveryhero.com>
2024-06-05 19:43:37 +0200

    added basic chatbot interaction using retrieval from data source

--- src/chat_app/chat.py
+++ src/chat_app/chat.py
@@ -1,7 +1,19 @@
 # Adapted from: https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps#introduction
 import streamlit as st
 
+from sports_chatbot.chatbot import SportsChatbot
+
 st.title("Sports News ChatBot")
+chatbot_name = "Sporty"
+# TODO: change this hardcoded data reference
+chatbot = SportsChatbot("./data/sports_chatbot/Day_1.csv")
+
+response = (
+    "Hello! My name is Sporty, how can I assist you in the world of sports today?"
+)
+# Display assistant response in chat message container
+with st.chat_message(chatbot_name):
+    st.markdown(response)
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -20,10 +32,10 @@ if prompt := st.chat_input("What is up?"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-
-response = f"Echo: {prompt}"
-# Display assistant response in chat message container
-with st.chat_message("assistant"):
-    st.markdown(response)
-# Add assistant response to chat history
-st.session_state.messages.append({"role": "assistant", "content": response})
+if prompt:
+    response = chatbot.query(prompt)["answer"]
+    # Display assistant response in chat message container
+    with st.chat_message(chatbot_name):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": chatbot_name, "content": response})
